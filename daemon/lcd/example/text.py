eee3b183631e31d489adf95bf5f75333f8e8ca3b
tk <iam@tiankun.me>
2016-09-24 11:45:49 +0800

    delete pcd8533 add lcd

---
+++ daemon/lcd/example/text.py
@@ -0,0 +1,107 @@
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import time
+
+import Adafruit_Nokia_LCD as LCD
+import Adafruit_GPIO.SPI as SPI
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+# Raspberry Pi hardware SPI config:
+# DC = 23
+# RST = 24
+# SPI_PORT = 0
+# SPI_DEVICE = 0
+
+# Raspberry Pi software SPI config:
+SCLK = 17
+DIN = 18
+DC = 27
+RST = 23
+CS = 22
+
+# Beaglebone Black hardware SPI config:
+# DC = 'P9_15'
+# RST = 'P9_12'
+# SPI_PORT = 1
+# SPI_DEVICE = 0
+
+# Beaglebone Black software SPI config:
+# DC = 'P9_15'
+# RST = 'P9_12'
+# SCLK = 'P8_7'
+# DIN = 'P8_9'
+# CS = 'P8_11'
+
+
+# Hardware SPI usage:
+# disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
+
+# Software SPI usage (defaults to bit-bang SPI interface):
+disp = LCD.PCD8544(DC, RST, SCLK, DIN, CS)
+
+# Initialize library.
+disp.begin(contrast=60)
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a white filled box to clear the image.
+draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Write some text.
+print 'Type text:'
+print 'You have 5 1/2 lines to write.'
+
+one =   raw_input("1 => ")
+two =   raw_input("2 => ")
+three = raw_input("3 => ")
+four =  raw_input("4 => ")
+five =  raw_input("5 => ")
+six =   raw_input("6 => ")
+
+draw.text((0,0),  one,   font=font)
+draw.text((0,8),  two,   font=font)
+draw.text((0,16), three, font=font)
+draw.text((0,24), four,  font=font)
+draw.text((0,32), five,  font=font)
+draw.text((0,40), six,   font=font)
+
+# Display image.
+disp.image(image)
+disp.display()
+
+quit()
